01540484b55673bbfd7c0510c95f263caa6ace7c
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-04-25 14:56:19 +0300

    added alert to home and shop

--- src/app/pages/home-page/home-page.page.ts
+++ src/app/pages/home-page/home-page.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {Cd,GetRamdonToPageService } from 'src/app/apis/get-ramdon-to-page.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home-page',
@@ -9,7 +10,7 @@ import {Cd,GetRamdonToPageService } from 'src/app/apis/get-ramdon-to-page.servic
 })
 export class HomePagePage implements OnInit {
 cd: Cd[];
-  constructor(private router: Router,private service: GetRamdonToPageService) { }
+  constructor(private router: Router,private service: GetRamdonToPageService, private alert: AlertController) { }
 
   ngOnInit() {
     this.service.getRandom().subscribe(response=>{
@@ -17,8 +18,17 @@ cd: Cd[];
       console.log(this.cd);
     });
   }
+  async getalert() {
+    const alert = await this.alert.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      message: 'Item Purchased!',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 buy(){
-  alert('This product is purchased');
+  this.getalert();
 }
 home(){
   this.router.navigate(['/home-page']);
--- src/app/pages/shop/shop.page.ts
+++ src/app/pages/shop/shop.page.ts
@@ -4,6 +4,7 @@ import {Cdps3,Getcdps3Service } from 'src/app/apis/getcdps3.service';
 import {Cdps4,Getcdps4Service } from 'src/app/apis/getcdps4.service';
 import {Cdps5 ,Getcdps5Service } from 'src/app/apis/getcdps5.service';
 import {Cdxbox ,GetcdxboxService } from 'src/app/apis/getcdxbox.service';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.page.html',
@@ -17,7 +18,7 @@ cdsxbox: Cdxbox[];
 
 
   // eslint-disable-next-line max-len
-  constructor(private router: Router, private serviceps3: Getcdps3Service,private serviceps4: Getcdps4Service, private serviceps5: Getcdps5Service, private servicexbox: GetcdxboxService) { }
+  constructor(private router: Router, private serviceps3: Getcdps3Service,private serviceps4: Getcdps4Service, private serviceps5: Getcdps5Service, private servicexbox: GetcdxboxService, private alert: AlertController) { }
 
   ngOnInit() {
     //ps3
@@ -42,8 +43,17 @@ cdsxbox: Cdxbox[];
     });
 
   }
+  async getalert() {
+    const alert = await this.alert.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      message: 'Item Purchased',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
   buy(){
-    alert('This product is purchased');
+    this.getalert();
   }
   home(){
     this.router.navigate(['/home-page']);
